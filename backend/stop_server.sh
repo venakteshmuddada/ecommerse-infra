fd10652b9f8554a3eb93d0ba198e4b4de4ca068d
venakteshmuddada <venkatesh.muddada2506@gmail.com>
2025-04-29 18:36:18 -0400

    Delete backend directory

--- backend/stop_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Stop the application
-pm2 stop ecommerce-app
-pm2 delete ecommerce-app
--- backend/validate_service.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Validate service is running
-if pm2 list | grep -q "ecommerce-app"; then
-  echo "Application is running"
-  exit 0
-else
-  echo "Application is not running"
-  exit 1
-fi
